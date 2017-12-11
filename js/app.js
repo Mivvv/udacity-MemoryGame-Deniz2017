7e76c89f086d41e648c80315eb7f7942c8f1484b
Mivvv <denizatun@gmail.com>
2017-12-11 11:14:42 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 /*
  * Create a list that holds all of your cards
  */
+ 
+var card_list = ["diamond","paper-plane-o","anchor","bolt","cube","leaf","bicycle","bomb"];
 
+var opened_cards = [];
+
+var matched_cards = [];
 
 /*
  * Display the cards on the page
@@ -9,6 +14,9 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+ 
+ 
+ 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,6 +33,15 @@ function shuffle(array) {
     return array;
 }
 
+deck_elements = card_list.concat(card_list); // Created the deck
+deck_elements = shuffle(deck_elements); // Shuffled the deck
+
+card_class = $('<li class="card"></li>');
+
+for (i=0; i< deck_elements.length; i++){
+	$('.deck').append('<li class="card"><i class="fa fa-'+deck_elements[i]+'"></i></li>');
+}	// add cards with HTML into deck class
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +53,45 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ $('.card').on('click', function(event){ 
+	if (event.target.className == "card"){
+		icon = event.target.childNodes[0];
+		card = event.target
+		
+		showCard(card);
+		
+		checkCards(card);
+
+	}
+ })
+
+ 
+function showCard(card){
+	card.className = "card open show";
+	opened_cards.push(card);
+	console.log(opened_cards);
+}
+ 
+function checkCards(card){
+	if (opened_cards.length > 1){
+		card1 = opened_cards[opened_cards.length - 1];
+		card2 = opened_cards[opened_cards.length - 2];
+		
+		if (card1.childNodes[0].className == card2.childNodes[0].className){
+			card1.className = "card match";
+			card2.className = "card match";
+			matched_cards.push(card1.childNodes[0].className);
+			opened_cards.pop();
+			opened_cards.pop();
+		}else {
+			card1.className = "card";
+			card2.className = "card";
+			opened_cards.pop();
+			opened_cards.pop();
+		}
+		
+	}
+}
+ 
+ 
