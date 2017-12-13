bd88ec3b8a3ff67a215b46bdf29b87c036d77393
Mivvv <denizatun@gmail.com>
2017-12-13 23:52:51 +0300

    Cleaned some stuff
    Restart function still seems messy... Well, I do plan to work on more visual updates like animations and style of the victory screen.
    
    Maybe I will return to this problem later, also I still haven't put clear commets

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
+////////////
+////13.12.2017
+//  The date skynet took over the humanity,
+//  our only salvation lies in this memory game.
+///////////
+// Jk, I've decided to tidy up the code, put everything into
+// meaningful functions.
+
+ 
 /*
- * Create a list that holds all of your cards
+ * Variables
  */
  
 var card_list = ["diamond","paper-plane-o","anchor","bolt","cube","leaf","bicycle","bomb"];
@@ -10,21 +19,14 @@ var matched_cards = [];
 
 var moves = 0;
 
-var stars = 4;
+var stars = 3;
 
 var timeExecute = false;
 
 var timeRestart = false;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
- 
- 
- 
+var timeUpdate = true;
+  
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -41,6 +43,7 @@ function shuffle(array) {
     return array;
 }
 
+// Time function FINALLY! https://stackoverflow.com/questions/29971898/how-to-create-an-accurate-timer-in-javascript
 function timer(){
 	
 	if (!timeExecute)
@@ -63,8 +66,10 @@ function timer(){
 			}
 					
 			var gameTime = " " + minute + ":" + second;
-				
-			$(".timey").text(gameTime);
+			
+			if (timeUpdate){
+				$(".timey").text(gameTime);
+			}
 			
 			if(timeRestart){
 				clearInterval(clocker);
@@ -86,43 +91,21 @@ function shuffleDeck(){
 	}	// add cards with HTML into deck class
 
 }
-
-deck_elements = card_list.concat(card_list); // Created the deck
-shuffleDeck()
-
-
+	 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * YOU KNOW YOU SHOULD SHOW THE WRONG CARD FOR 1SEC OR SOMETHING AND THEN CLOSE IT
+ * THAT IS HOW THE GAME IS PLAYED, IT IS NOT A RUSSIAN ROULETTE!
  */
-
- $('.card').on('click', function(event){ 
-	if (event.target.className == "card"){
-		icon = event.target.childNodes[0];
-		card = event.target
-		
-		showCard(card);
-		
-		checkCards(card);
-		
-		timer();
-	}
- })
-
- 
 function showCard(card){
 	card.className = "card open show";
 	opened_cards.push(card);
 }
+
+
  
 function checkCards(card){
 	if (opened_cards.length > 1){
+		
 		card1 = opened_cards[opened_cards.length - 1];
 		card2 = opened_cards[opened_cards.length - 2];
 		
@@ -141,25 +124,26 @@ function checkCards(card){
 		}else {
 			card1.className = "card";
 			card2.className = "card";
+			
 			opened_cards.pop();
 			opened_cards.pop();
 		}
 		
 	}
 }
- 
- 
+
 function moveCounter(){
 	moves++;
 	$('.moves').text(moves);
+	
+	scoreStars();
 }
 
 function scoreStars(){
-	if (moves == 8){
+
+	if ( moves == 8 ){
 		stars = 4;
-	}
-	else if ( moves >= 8 && moves <= 12){
-		
+	else if ( moves > 8 && moves <= 12){
 		stars = 3;
 	}else if ( moves >=12 && moves <=17){
 		$('.stars li:last-child .fa').removeClass("fa-star");
@@ -192,21 +176,36 @@ function popupVictory() {
 		} 
 
 }
+ 
+function clicky(event){
+	if (event.target.className == "card"){
+			icon = event.target.childNodes[0];
+			card = event.target
+			
+			showCard(card);
+			
+			checkCards(card);
+			
+			timer();
 
-/* https://stackoverflow.com/questions/17650776/add-remove-html-inside-div-using-javascript
+		}
+}
+ 
+ /* https://stackoverflow.com/questions/17650776/add-remove-html-inside-div-using-javascript
  *  link helped me a lot to add messages/stars to victory screen
  */
-
+  
  function checkVictory(){
-	if (matched_cards.length == card_list.length){
-			 i = 0;
-			 for(i=0;i < stars; i++){
-				 starScore = document.createElement('i');
-				 starScore.className = "fa fa-star";
-				 document.getElementById('star-score').appendChild(starScore);
-		 		}
-		para = document.getElementById("custom-message");
-		switch(stars){
+	 if (matched_cards.length == card_list.length){
+		 i = 0;
+		 for(i=0;i < stars; i++){
+			 starScore = document.createElement('i');
+			 starScore.className = "fa fa-star";
+			 document.getElementById('star-score').appendChild(starScore);
+		 }
+		 
+		 para = document.getElementById("custom-message");
+		 switch(stars){
 			 case 0:
 				text0 = document.createTextNode("Try harder next time!");
 				para.appendChild(text0);
@@ -224,46 +223,59 @@ function popupVictory() {
 				para.appendChild(text3);
 				 break;
 			 case 4:
-				text4 = document.createTextNode("Either you are cheating, or too damn lucky!");
+				text4 = document.createTextNode("Either you are cheating or you are very very very very lucky person!");
 				para.appendChild(text4);
-				break;
+				 break;
 			 case -1:
 				textMinus = document.createTextNode("Dude.... Really?");
 				para.appendChild(textMinus);
 				break;
-		 		}
+		 }
+		 timeUpdate = false;
 		 popupVictory();
 	 }
  }
+ 
 
+ 
  function restart(){
 	
 	opened_cards = [];
 	matched_cards = [];
+	
+	timeExecute = false;
+	timeRestart = true;
+	timeUpdate = true;
+	
 	moves = 0;
 	$('.moves').text(moves);
+	
 	stars = 3;
 	$('.stars i').removeClass("fa-star-o");
 	$('.stars i').addClass("fa-star");
 	
 	shuffleDeck();
-	 
-	$('.card').on('click', function(event){ 
-		if (event.target.className == "card"){
-			icon = event.target.childNodes[0];
-			card = event.target
-			
-			showCard(card);
-			
-			checkCards(card);
-			
-			timer();
-		}
-	 })
-	 
-    var popup = document.getElementById('victory-container');
+	
+	$('.card').on('click', function(event){
+		clicky(event);
+	});
+		
+	
+	var popup = document.getElementById('victory-container');
     popup.style.display = "none";
 }
+
+/*
+ * Starting point of inital function calls and
+ * some events or stuff that are triggered.
+ */
+
+deck_elements = card_list.concat(card_list); // Created the deck
+shuffleDeck()
+
+$('.card').on('click', function(event){
+		clicky(event);
+});
 
 $('.restart').on('click', function (event) {
     event.preventDefault();
